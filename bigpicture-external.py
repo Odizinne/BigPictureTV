bdc194af49c6f83f6f1986474c219afb4a9bf02c
Odizinne <flora.aubry@proton.me>
2024-05-02 08:49:07 +0200

    Update bigpicture-external.py

--- bigpicture-external.py
+++ bigpicture-external.py
@@ -1,27 +1,26 @@
 import subprocess
 import time
 import pygetwindow as gw
-import json
-import os
+from utils import pyaudiodevices
 
-CONFIG_FILE = "config.json"
+gamemode_audio = "SAMSUNG"
+gamemode_screen = "/external"
+desktop_audio = "Headset Earphone"
+desktop_screen = "/internal"
+gamemode = None
+bigpicture_keywords = 'Steam', 'mode', 'Big', 'Picture'
 
-def read_config(file_path):
-    with open(file_path, 'r') as config_file:
-        config = json.load(config_file)
-    return config
+def switch_audio(audio_output):
+    devices = pyaudiodevices.get_audio_devices()
+    pyaudiodevices.set_audio_device(audio_output, devices)
 
-def write_default_config(file_path):
-    default_config = {
-        "//": "Please specify the audio output device names in this config file",
-        "gamemode_audio": "",
-        "desktop_audio": ""
-    }
-    with open(file_path, 'w') as config_file:
-        json.dump(default_config, config_file, indent=4)
+def switch_screen(mode):
+    print(f"Switch screen to {mode}.")
+    subprocess.run(["DisplaySwitch.exe", mode])
 
-def set_audio_device(audio_output):
-    subprocess.call(["pythonw", "pyaudiodevices.py", "-d", audio_output])
+def startup_reset():
+    switch_screen(desktop_screen)
+    switch_audio(desktop_audio)
 
 def is_steam_big_picture_open(keywords):
     windows = gw.getAllTitles()
@@ -30,31 +29,17 @@ def is_steam_big_picture_open(keywords):
             return True
     return False
 
-if not os.path.exists(CONFIG_FILE):
-    print("Configuration file doesn't exist. Creating default configuration...")
-    write_default_config(CONFIG_FILE)
-    print("Default configuration created in 'config.json'. Please edit this file with your audio outputs.")
-    exit()
+startup_reset()
 
-if os.stat(CONFIG_FILE).st_size == 0:
-    print("Configuration file is empty. Script cannot continue without audio settings.")
-    exit()
-
-config = read_config(CONFIG_FILE)
-gamemode_audio = config["gamemode_audio"]
-desktop_audio = config["desktop_audio"]
-gamemode = None
 try:
     while True:
-        if is_steam_big_picture_open(['Steam', 'mode', 'Big', 'Picture']) and not gamemode:
-            print("Steam Big Picture mode is open.")
-            subprocess.run(["DisplaySwitch.exe", "/external"])
-            set_audio_device(gamemode_audio)
+        if is_steam_big_picture_open(bigpicture_keywords) and not gamemode:
+            switch_screen(gamemode_screen)
+            switch_audio(gamemode_audio)
             gamemode = True
-        elif not is_steam_big_picture_open(['Steam', 'mode', 'Big', 'Picture']) and gamemode:
-            print("Steam Big Picture mode is not open.")
-            subprocess.run(["DisplaySwitch.exe", "/internal"])
-            set_audio_device(desktop_audio)
+        elif not is_steam_big_picture_open(bigpicture_keywords) and gamemode:
+            switch_screen(desktop_screen)
+            switch_audio(desktop_audio)
             gamemode = False
         time.sleep(1) 
 except KeyboardInterrupt:
