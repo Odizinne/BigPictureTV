f7f09e0e2fed16366f7d17d52924988467dca5cf
Odizinne <flora.aubry@proton.me>
2024-05-02 17:23:30 +0200

    refactor

--- bigpicture-external.py
+++ bigpicture-external.py
@@ -3,26 +3,25 @@ import time
 import pygetwindow as gw
 from utils import pyaudiodevices
 
-gamemode_audio = "SAMSUNG"
-gamemode_screen = "/external"
-desktop_audio = "Headset Earphone"
-desktop_screen = "/internal"
-gamemode = None
-bigpicture_keywords = 'Steam', 'mode', 'Big', 'Picture'
+BIG_PICTURE_KEYWORDS = ['Steam', 'mode', 'Big', 'Picture']
+GAMEMODE_AUDIO = "SAMSUNG"
+DESKTOP_AUDIO = "Headset Earphone"
+GAMEMODE_SCREEN = "/external"
+DESKTOP_SCREEN = "/internal"
+GAMEMODE = None
 
 def switch_audio(audio_output):
     devices = pyaudiodevices.get_audio_devices()
     pyaudiodevices.set_audio_device(audio_output, devices)
 
 def switch_screen(mode):
-    print(f"Switch screen to {mode}.")
     subprocess.run(["DisplaySwitch.exe", mode])
 
 def startup_reset():
-    switch_screen(desktop_screen)
-    switch_audio(desktop_audio)
+    switch_screen(DESKTOP_SCREEN)
+    switch_audio(DESKTOP_AUDIO)
 
-def is_steam_big_picture_open(keywords):
+def bigpicture_detector(keywords):
     windows = gw.getAllTitles()
     for window_title in windows:
         if all(word in window_title for word in keywords):
@@ -33,14 +32,14 @@ startup_reset()
 
 try:
     while True:
-        if is_steam_big_picture_open(bigpicture_keywords) and not gamemode:
-            switch_screen(gamemode_screen)
-            switch_audio(gamemode_audio)
-            gamemode = True
-        elif not is_steam_big_picture_open(bigpicture_keywords) and gamemode:
-            switch_screen(desktop_screen)
-            switch_audio(desktop_audio)
-            gamemode = False
+        if bigpicture_detector(BIG_PICTURE_KEYWORDS) and not GAMEMODE:
+            switch_screen(GAMEMODE_SCREEN)
+            switch_audio(GAMEMODE_AUDIO)
+            GAMEMODE = True
+        elif not bigpicture_detector(BIG_PICTURE_KEYWORDS) and GAMEMODE:
+            switch_screen(DESKTOP_SCREEN)
+            switch_audio(DESKTOP_AUDIO)
+            GAMEMODE = False
         time.sleep(1) 
 except KeyboardInterrupt:
     print("\nExiting")
