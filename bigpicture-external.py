0164068d414ef981285ab7da8bfca5587670f9a6
Odizinne <flora.aubry@proton.me>
2024-05-05 10:40:57 +0200

    refactor

--- bigpicture-external.py
+++ bigpicture-external.py
@@ -5,18 +5,18 @@ import pygetwindow as gw
 BIG_PICTURE_KEYWORDS = ['Steam', 'mode', 'Big', 'Picture']
 GAMEMODE_SCREEN = "/external"
 DESKTOP_SCREEN = "/internal"
-gamemode = None
+gamemode = False
 
 def switch_screen(mode):
-    subprocess.run(["DisplaySwitch.exe", mode])
+    subprocess.run(["DisplaySwitch.exe", mode], check=True)
 
 def startup_reset():
     switch_screen(DESKTOP_SCREEN)
 
-def bigpicture_detector(keywords):
+def is_bigpicture_running():
     windows = gw.getAllTitles()
     for window_title in windows:
-        if all(word in window_title for word in keywords):
+        if all(word in window_title for word in BIG_PICTURE_KEYWORDS):
             return True
     return False
 
@@ -24,10 +24,11 @@ startup_reset()
 
 try:
     while True:
-        if bigpicture_detector(BIG_PICTURE_KEYWORDS) and not gamemode:
+        bigpicture = is_bigpicture_running()
+        if bigpicture and not gamemode:
             switch_screen(GAMEMODE_SCREEN)
             gamemode = True
-        elif not bigpicture_detector(BIG_PICTURE_KEYWORDS) and gamemode:
+        elif not bigpicture and gamemode:
             switch_screen(DESKTOP_SCREEN)
             gamemode = False
         time.sleep(1) 
