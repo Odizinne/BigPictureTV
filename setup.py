26a55e7fc2966f4fcb232cba870e31c58a89eeab
Odizinne <flora.aubry@proton.me>
2024-07-06 22:14:39 +0200

    Update build-main.yml

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from cx_Freeze import setup, Executable
 
 build_dir = "build/BigPictureTV"
 base = "Win32GUI"
-system_icon = "src/icons/steamos-logo.ico"
+system_icon = "icons/steamos-logo.ico"
 
-zip_include_packages = ['PyQt6', 'winshell', 'PyGetWindow']
+zip_include_packages = ['PyQt6', 'winshell', 'PyGetWindow', 'pywin32']
 
 build_exe_options = {
-    "include_files": [('src/icons/steamos-logo.png', 'src/icons/steamos-logo.png')],
+    "include_files": [('icons/steamos-logo.png', 'icons/steamos-logo.png')],
     "build_exe": build_dir,
     "zip_include_packages": zip_include_packages,   
 }
 
 executables = [
-    Executable('src/BigPictureTV.py', base=base, icon=system_icon)
+    Executable('BigPictureTV.py', base=base, icon=system_icon)
 ]
 
 setup(
